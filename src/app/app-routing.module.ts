f3093f5236589e9108a02ed015616896c6769cb8
WIllianMartins2018 <willian.martins01@hotmail.com>
2023-07-25 20:20:48 -0300

    feature/reorganizacao-pastas-e-add-rotas-do-sistema

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,42 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './pages/account/login-page/login-page.component';
+import { FramePageComponent } from './pages/master/frame-page.component';
+import { ProductsPageComponent } from './pages/store/products-page/products-page.component';
+import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
+import { PetsPageComponent } from './pages/account/pets-page/pets-page.component';
+import { SingupPageComponent } from './pages/account/singup-page/singup-page.component';
+import { ResetPasswordPageComponent } from './pages/account/reset-password-page/reset-password-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: FramePageComponent,
+    children: [
+      {path : '', component: ProductsPageComponent},
+      {path: 'cart', component: CartPageComponent}
+    ]
+  },
+  {
+    path: 'account',
+    component: FramePageComponent,
+    children: [
+      {path: 'pets', component: PetsPageComponent}
+    ]
+  },
+  {
+    path: 'login',
+    component: LoginPageComponent
+  },
+  {
+    path: 'singup',
+    component: SingupPageComponent
+  },
+  {
+    path: 'reset-password',
+    component: ResetPasswordPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { LoginPageComponent } from './login-page/login-page.component';
-import { ResetPasswordPageComponent } from './reset-password-page/reset-password-page.component';
-import { SingupPageComponent } from './singup-page/singup-page.component';
-import { PetsPageComponent } from './pets-page/pets-page.component';
-import { ProductsPageComponent } from './products-page/products-page.component';
-import { CartPageComponent } from './cart-page/cart-page.component';
+import { LoginPageComponent } from './pages/account/login-page/login-page.component'
+import { ResetPasswordPageComponent } from './pages/account/reset-password-page/reset-password-page.component';
+import { SingupPageComponent } from './pages/account/singup-page/singup-page.component';
+import { PetsPageComponent } from './pages/account/pets-page/pets-page.component';
+import { ProductsPageComponent } from './pages/store/products-page/products-page.component';
+import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
+import { FramePageComponent } from './pages/master/frame-page.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { CartPageComponent } from './cart-page/cart-page.component';
     SingupPageComponent,
     PetsPageComponent,
     ProductsPageComponent,
-    CartPageComponent
+    CartPageComponent,
+    FramePageComponent
   ],
   imports: [
     BrowserModule,
